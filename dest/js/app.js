4daee550ee58a2f3175ff4178417a2841caf6ef3
Andrii <Luckyhidalgo@gmail.com>
2021-03-10 21:59:52 +0200

    Homepage::  - created solution section - w\o adaptive;

--- dest/js/app.js
+++ dest/js/app.js
@@ -34,6 +34,28 @@ var initPreventBehavior = function initPreventBehavior() {
 	});
 };
 
+/**
+ * @name initSwiper
+ *
+ * @description initialize Swiper
+ */
+var initSwiper = function initSwiper() {
+
+	for (var i = 0, j = 1; i < $('.solution__slider-wrapper-1 .solution__slider-row').length; i++, j++) {
+		new Swiper('.solutionSlider1' + i, {
+			effect: 'flip',
+			flipEffect: {
+				slideShadows: false
+			},
+			speed: 1000,
+			navigation: {
+				nextEl: '.solution__slider-wrapper-1 .solution__slider-row-' + j + ' .solution__slider-btn--next',
+				prevEl: '.solution__slider-wrapper-1 .solution__slider-row-' + j + ' .solution__slider-btn--prev'
+			}
+		});
+	}
+};
+
 /*
 * CALLBACK :: start
 * ============================================= */
@@ -48,6 +70,76 @@ var approachTabCB = function approachTabCB() {
 		$('.approach__tab-content').hide();
 		$('.approach__tab-content[data-tab-content="' + elID + '"]').fadeIn(550);
 	});
+};
+
+var solutionCB = function solutionCB() {
+	function helperSlider(btnName, footerNode) {
+		$(btnName).on('click', function (ev) {
+			var el = $(ev.currentTarget),
+			    elID = el.attr('data-id');
+
+			$(btnName).removeClass('is-active');
+			el.addClass('is-active');
+
+			$(footerNode).removeClass('is-show');
+			$(footerNode + '-' + elID).addClass('is-show');
+		});
+	}
+
+	var partBool2 = true,
+	    partBool3 = true;
+
+	$('.solution__tab').on('click', function (ev) {
+		var el = $(ev.currentTarget),
+		    elID = el.attr('data-tab-id');
+
+		$('.solution__tab').removeClass('is-active');
+		el.addClass('is-active');
+
+		$('.solution__tab-content').hide();
+		$('.solution__tab-content[data-tab-content="' + elID + '"]').fadeIn(550);
+
+		setTimeout(function () {
+
+			if (parseInt(elID) === 1 && partBool2) {
+				for (var i = 0, j = 1; i < $('.solution__slider-wrapper-2 .solution__slider-row').length; i++, j++) {
+					new Swiper('.solutionSlider2' + i, {
+						effect: 'flip',
+						flipEffect: {
+							slideShadows: false
+						},
+						speed: 1000,
+						navigation: {
+							nextEl: '.solution__slider-wrapper-2 .solution__slider-row-' + j + ' .solution__slider-btn--next',
+							prevEl: '.solution__slider-wrapper-2 .solution__slider-row-' + j + ' .solution__slider-btn--prev'
+						}
+					});
+				}
+
+				partBool2 = false;
+			} else if (parseInt(elID) === 2 && partBool3) {
+				for (var _i = 0, _j = 1; _i < $('.solution__slider-wrapper-3 .solution__slider-row').length; _i++, _j++) {
+					new Swiper('.solutionSlider3' + _i, {
+						effect: 'flip',
+						flipEffect: {
+							slideShadows: false
+						},
+						speed: 1000,
+						navigation: {
+							nextEl: '.solution__slider-wrapper-3 .solution__slider-row-' + _j + ' .solution__slider-btn--next',
+							prevEl: '.solution__slider-wrapper-3 .solution__slider-row-' + _j + ' .solution__slider-btn--prev'
+						}
+					});
+				}
+
+				partBool3 = false;
+			}
+		}, 100);
+	});
+
+	helperSlider('.solution__slider-head-1 a', '.solution__slider-footer-1 .solution__slider-row');
+	helperSlider('.solution__slider-head-2 a', '.solution__slider-footer-2 .solution__slider-row');
+	helperSlider('.solution__slider-head-3 a', '.solution__slider-footer-3 .solution__slider-row');
 };
 /*
 * CALLBACK :: end
@@ -64,10 +156,12 @@ var initNative = function initNative() {
 	// ==========================================
 
 	// lib
-	approachTabCB();
+	initSwiper();
 	// ==========================================
 
 	// callback
+	approachTabCB();
+	solutionCB();
 	// ==========================================
 };
 
--- src/js/_document/indexNative.js
+++ src/js/_document/indexNative.js
@@ -13,6 +13,79 @@ const approachTabCB = () => {
 		$('.approach__tab-content[data-tab-content="' + elID + '"]').fadeIn(550);
 	});
 };
+
+
+const solutionCB = () => {
+	function helperSlider(btnName, footerNode) {
+		$(btnName).on('click', (ev) => {
+			const el = $(ev.currentTarget),
+				elID = el.attr('data-id');
+			
+			$(btnName).removeClass('is-active');
+			el.addClass('is-active');
+			
+			$(footerNode).removeClass('is-show');
+			$(footerNode + '-' +  elID).addClass('is-show');
+		});
+	}
+	
+	
+	let partBool2 = true,
+		partBool3 = true;
+	
+	$('.solution__tab').on('click', (ev) => {
+		const el = $(ev.currentTarget),
+			elID = el.attr('data-tab-id');
+		
+		$('.solution__tab').removeClass('is-active');
+		el.addClass('is-active');
+		
+		$('.solution__tab-content').hide();
+		$('.solution__tab-content[data-tab-content="' + elID + '"]').fadeIn(550);
+		
+		setTimeout(() => {
+			
+			if(parseInt(elID) === 1 && partBool2) {
+				for(let i = 0, j = 1; i < $('.solution__slider-wrapper-2 .solution__slider-row').length; i++, j++) {
+					new Swiper('.solutionSlider2' + i, {
+						effect: 'flip',
+						flipEffect: {
+							slideShadows: false,
+						},
+						speed: 1000,
+						navigation: {
+							nextEl: '.solution__slider-wrapper-2 .solution__slider-row-' + j + ' .solution__slider-btn--next',
+							prevEl: '.solution__slider-wrapper-2 .solution__slider-row-' + j + ' .solution__slider-btn--prev',
+						}
+					});
+				}
+				
+				partBool2 = false;
+			} else if(parseInt(elID) === 2 && partBool3) {
+				for(let i = 0, j = 1; i < $('.solution__slider-wrapper-3 .solution__slider-row').length; i++, j++) {
+					new Swiper('.solutionSlider3' + i, {
+						effect: 'flip',
+						flipEffect: {
+							slideShadows: false,
+						},
+						speed: 1000,
+						navigation: {
+							nextEl: '.solution__slider-wrapper-3 .solution__slider-row-' + j + ' .solution__slider-btn--next',
+							prevEl: '.solution__slider-wrapper-3 .solution__slider-row-' + j + ' .solution__slider-btn--prev',
+						}
+					});
+				}
+				
+				partBool3 = false;
+			}
+			
+		}, 100);
+	})
+	
+	helperSlider('.solution__slider-head-1 a','.solution__slider-footer-1 .solution__slider-row');
+	helperSlider('.solution__slider-head-2 a','.solution__slider-footer-2 .solution__slider-row');
+	helperSlider('.solution__slider-head-3 a','.solution__slider-footer-3 .solution__slider-row');
+};
 /*
 * CALLBACK :: end
 * ============================================= */
@@ -29,10 +102,12 @@ const initNative = () => {
 	// ==========================================
 	
 	// lib
-	approachTabCB();
+	initSwiper();
 	// ==========================================
 	
 	// callback
+	approachTabCB();
+	solutionCB();
 	// ==========================================
 };
 
