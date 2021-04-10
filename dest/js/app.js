0444091bf3d1e3793b606979fa283f57a151d1d6
Andrii <Luckyhidalgo@gmail.com>
2021-04-10 16:06:20 +0300

    Home page :::  - full adaptive;

--- dest/js/app.js
+++ dest/js/app.js
@@ -377,36 +377,55 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 
 	$('.main__bg').css({ opacity: 1 });
 
-	if (vid1.paused) vid1.play();
-
 	/* SCENE 1
  * ==================== */
-	var tween1 = new TimelineMax(),
+	var tween1 = null,
 	    scene1 = null,
-	    controller1 = new ScrollMagic.Controller();
+	    controller1 = null;
 
-	var laptopDimension = $('#laptop-fill')[0].getBoundingClientRect(),
-	    laptopDimensionW = laptopDimension.width - 5,
-	    laptopDimensionH = laptopDimension.height - 5;
+	function helperScene1() {
+		tween1 = new TimelineMax();
+		controller1 = new ScrollMagic.Controller();
 
-	tween1.fromTo('#video1WrapperBorder > i', 0.5, {
-		width: '50vw',
-		height: '100vh',
-		borderRadius: 0,
-		boxShadow: '0px 5px 15px rgba(64, 62, 61, 0)', transformOrigin: 'center' }, {
-		width: laptopDimensionW,
-		height: laptopDimensionH,
-		borderRadius: 12,
-		boxShadow: '0px 5px 15px rgba(64, 62, 61, 0.3)', ease: Linear.easeNone });
+		var laptopDimension = $('#laptop-fill')[0].getBoundingClientRect(),
+		    laptopDimensionW = laptopDimension.width - 5,
+		    laptopDimensionH = laptopDimension.height - 5;
 
-	scene1 = new ScrollMagic.Scene({
-		triggerElement: ".main__wrapper-5",
-		offset: $(window).outerHeight(true) / 2 + 10,
-		duration: $('.main__wrapper-5').outerHeight(true) - ($(window).outerHeight(true) / 2 + 10)
-	}).setTween(tween1)
-	// .addIndicators({name: 'video'})
-	.addTo(controller1);
+		tween1.fromTo('#video1WrapperBorder > i', 0.5, {
+			width: '50vw',
+			height: '100vh',
+			borderRadius: 0,
+			boxShadow: '0px 5px 15px rgba(64, 62, 61, 0)', transformOrigin: 'center' }, {
+			width: laptopDimensionW,
+			height: laptopDimensionH,
+			borderRadius: 12,
+			boxShadow: '0px 5px 15px rgba(64, 62, 61, 0.3)', ease: Linear.easeNone });
 
+		scene1 = new ScrollMagic.Scene({
+			triggerElement: ".main__wrapper-5",
+			offset: $(window).outerHeight(true) / 2 + 10,
+			duration: $('.main__wrapper-5').outerHeight(true) - ($(window).outerHeight(true) / 2 + 10)
+		}).setTween(tween1)
+		// .addIndicators({name: 'video'})
+		.addTo(controller1);
+	}
+
+	if (document.documentElement.clientWidth >= 768) {
+		helperScene1();
+	}
+
+	window.addEventListener("resize", function () {
+		if (document.documentElement.clientWidth >= 768) {
+			helperScene1();
+		} else {
+			destroyController();
+		}
+	});
+
+	function destroyController() {
+		controller1.destroy(true);
+		scene1.destroy(true);
+	}
 	/* end :: SCENE 1
  * ==================== */
 
@@ -448,9 +467,6 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 	.addTo(controller21).on("progress", function (event) {
 		var num = Number((event.progress * 100).toFixed(0));
 
-		// console.log(`num: `, num);
-		// console.log(`vid1: `, vid1);
-
 		if (num >= 60) {
 			vid1.pause();
 			vid1.currentTime = 0;
@@ -472,80 +488,15 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 	/* SCENE 3
  * ==================== */
 	var tween3 = new TimelineMax(),
-	    tween31 = new TimelineMax(),
-	    tween32 = new TimelineMax(),
 	    scene3 = null,
-	    scene31 = null,
-	    scene32 = null,
 	    controller3 = new ScrollMagic.Controller(),
-	    controller31 = new ScrollMagic.Controller(),
-	    controller32 = new ScrollMagic.Controller(),
 	    vid2 = $('#video2')[0],
 	    vidBool = true;
 
 	var wrapperNode3 = $('.main__wrapper-3'),
 	    wrapperNode3H = wrapperNode3.outerHeight(true);
 
-	tween3.to(['#video1WrapperBorder > i', '#mainSVG1'], 0.5, { opacity: 0, ease: Power1.easeInOut }, '-=0').fromTo('#video2Wrapper', 1, { opacity: 0 }, { opacity: 1, ease: Power1.easeInOut }, '-=0.5')
-	// .to(['#laptop-screen', '#laptop-fill'], 1,
-	// 	{opacity: 0, ease: Linear.easeNone}, '-=1'
-	// )
-	// .to('#laptop-border', 1,
-	// 	{opacity: 0, ease: Linear.easeNone}, '-=1'
-	// )
-	// .to('#laptop-shadow', 1,
-	// 	{opacity: 0, y: -10, scale: 0, ease: Linear.easeNone}, '-=1'
-	// )
-	// .to('#laptop-line', 1,
-	// 	{opacity: 0, ease: Linear.easeNone}, '-=1'
-	// )
-	// .to('#laptop-bottom', 1,
-	// 	{opacity: 0, scaleX: 0.5, scaleY: 0, ease: Linear.easeNone}, '-=1'
-	// )
-	// .to('#mainSVG1 .text', 1,
-	// 	{opacity: 0, ease: Linear.easeNone}, '-=1'
-	// )
-	;
-
-	// tween31
-	// 	.fromTo(['#device-border', '#device-fill'], 1,
-	// 		{opacity: 0, x: 10, y: 30, scale: 0.7, transformOrigin: 'center'}, {opacity: 1, x: 0, y: 0, scale: 1, ease: Linear.easeNone}, '-=0'
-	// 	)
-	// 	.fromTo('#device-option', 1,
-	// 		{opacity: 0}, {opacity: 1, ease: Linear.easeNone}, '-=0'
-	// 	)
-	// ;
-
-	// tween32
-	// 	.fromTo('#device-circle-1', 10,
-	// 		{opacity: 0, x: -80, y: -93, transformOrigin: 'center'}, {opacity: 1, x: 0, y: 0, ease: Linear.easeNone}, '-=0')
-	// 	.to('#laptop-circle-1', 0.5,
-	// 		{opacity: 0, ease: Linear.easeNone}, '-=9.9'
-	// 	)
-	// 	.fromTo('#device-circle-2', 10,
-	// 		{opacity: 0, x: -113, y: -93, transformOrigin: 'center'}, {opacity: 1, x: 0, y: 0, ease: Linear.easeNone}, '-=0')
-	// 	.to('#laptop-circle-2', 0.5,
-	// 		{opacity: 0, ease: Linear.easeNone}, '-=10'
-	// 	)
-	// 	.fromTo('#device-circle-3', 10,
-	// 		{opacity: 0, x: -145, y: -93, transformOrigin: 'center'}, {opacity: 1, x: 0, y: 0, ease: Linear.easeNone}, '-=0')
-	// 	.to('#laptop-circle-3', 0.5,
-	// 		{opacity: 0, ease: Linear.easeNone}, '-=10'
-	// 	)
-	// 	.fromTo('#device-circle-4', 10,
-	// 		{opacity: 0, x: 282, y: -220, transformOrigin: 'center'}, {opacity: 1, x: 0, y: 0, ease: Linear.easeNone}, '-=0')
-	// 	.to('#laptop-circle-4', 0.5,
-	// 		{opacity: 0, ease: Linear.easeNone}, '-=10'
-	// 	)
-	// 	.fromTo('#device-circle-5', 10,
-	// 		{opacity: 0, x: 250, y: -220, transformOrigin: 'center'}, {opacity: 1, x: 0, y: 0, ease: Linear.easeNone}, '-=0')
-	// 	.to('#laptop-circle-5', 0.5,
-	// 		{opacity: 0, ease: Linear.easeNone}, '-=10'
-	// 	)
-	// 	.fromTo('#device-circle-6', 10,
-	// 		{opacity: 0, x: -153, y: 0, transformOrigin: 'center'}, {opacity: 1, x: 0, y: 0, ease: Linear.easeNone}, '-=0'
-	// 	)
-	// ;
+	tween3.to(['#video1WrapperBorder > i', '#mainSVG1'], 0.5, { opacity: 0, ease: Power1.easeInOut }, '-=0').fromTo('#video2Wrapper', 1, { opacity: 0 }, { opacity: 1, ease: Power1.easeInOut }, '-=0.5');
 
 	scene3 = new ScrollMagic.Scene({
 		triggerElement: ".main__wrapper-3",
@@ -555,8 +506,6 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 	// .addIndicators({name: 'video-laptop'})
 	.addTo(controller3).on("progress", function (event) {
 		var num = Number((event.progress * 100).toFixed(0));
-
-		// console.log(`scene3 num: `, num);
 
 		if (num >= 0 && vidBool) {
 			if (vid2.paused) vid2.play();
@@ -568,23 +517,6 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 		}
 	});
 
-	// scene31 = new ScrollMagic.Scene({
-	// 	triggerElement: ".main__wrapper-3",
-	// 	offset: 0,
-	// 	duration: wrapperNode3H / 3
-	// })
-	// 	.setTween(tween31)
-	// 	// .addIndicators({name: 'device'})
-	// 	.addTo(controller31);
-	//
-	// scene32 = new ScrollMagic.Scene({
-	// 	triggerElement: ".main__wrapper-3",
-	// 	offset: wrapperNode3H / 3,
-	// 	duration: (wrapperNode3H - (wrapperNode3H / 3)) - 100
-	// })
-	// 	.setTween(tween32)
-	// 	// .addIndicators({name: 'device-laptop-circle'})
-	// 	.addTo(controller32);
 	/* end :: SCENE 3
  * ==================== */
 
@@ -603,11 +535,7 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 	var wrapperNode4 = $('.main__wrapper-2'),
 	    wrapperNode4H = wrapperNode4.outerHeight(true);
 
-	tween4
-	// .to(['#device-border', '#device-fill', '#device-option'], 1,
-	// 	{opacity: 0, ease: Linear.easeNone}, '-=0'
-	// )
-	.fromTo('#video2Wrapper', 0.5, { opacity: 1 }, { opacity: 0, ease: Power1.easeInOut }, '-=0');
+	tween4.fromTo('#video2Wrapper', 0.5, { opacity: 1 }, { opacity: 0, ease: Power1.easeInOut }, '-=0');
 
 	tween41.fromTo('#anim4-bg rect', 0.5, {
 		opacity: 0,
@@ -623,31 +551,7 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 		ease: Linear.easeNone
 	}, '-=0.5');
 
-	tween42.fromTo('#anim4-box-1', 1, { opacity: 0, x: 14, y: 76, scale: 0.5, transformOrigin: 'center' }, { opacity: 1, x: 0, y: 0, scale: 1, ease: Bounce.easeOut }, '-=0')
-	// .to('#device-circle-1', 1,
-	// 	{opacity: 0, ease: Linear.easeNone}, '-=0.9'
-	// )
-	.fromTo('#anim4-box-2', 1, { opacity: 0, x: -23, y: 76, scale: 0.5, transformOrigin: 'center' }, { opacity: 1, x: 0, y: 0, scale: 1, ease: Bounce.easeOut }, '-=0')
-	// .to('#device-circle-2', 1,
-	// 	{opacity: 0, ease: Linear.easeNone}, '-=1'
-	// )
-	.fromTo('#anim4-box-3', 1, { opacity: 0, x: -58, y: 76, scale: 0.5, transformOrigin: 'center' }, { opacity: 1, x: 0, y: 0, scale: 1, ease: Bounce.easeOut }, '-=0')
-	// .to('#device-circle-3', 1,
-	// 	{opacity: 0, ease: Linear.easeNone}, '-=1'
-	// )
-	.fromTo('#anim4-box-4', 1, { opacity: 0, x: 62, y: 11, scale: 0.5, transformOrigin: 'center' }, { opacity: 1, x: 0, y: 0, scale: 1, ease: Bounce.easeOut }, '-=0')
-	// .to('#device-circle-4', 1,
-	// 	{opacity: 0, ease: Linear.easeNone}, '-=1'
-	// )
-	.fromTo('#anim4-box-5', 1, { opacity: 0, x: 27, y: 11, scale: 0.5, transformOrigin: 'center' }, { opacity: 1, x: 0, y: 0, scale: 1, ease: Bounce.easeOut }, '-=0')
-	// .to('#device-circle-5', 1,
-	// 	{opacity: 0, ease: Linear.easeNone}, '-=1'
-	// )
-	.fromTo('#anim4-box-6', 1, { opacity: 0, x: -9, y: 11, scale: 0.5, transformOrigin: 'center' }, { opacity: 1, x: 0, y: 0, scale: 1, ease: Bounce.easeOut }, '-=0')
-	// .to('#device-circle-6', 1,
-	// 	{opacity: 0, ease: Linear.easeNone}, '-=1'
-	// )
-	;
+	tween42.fromTo('#anim4-box-1', 1, { opacity: 0, x: 14, y: 76, scale: 0.5, transformOrigin: 'center' }, { opacity: 1, x: 0, y: 0, scale: 1, ease: Bounce.easeOut }, '-=0').fromTo('#anim4-box-2', 1, { opacity: 0, x: -23, y: 76, scale: 0.5, transformOrigin: 'center' }, { opacity: 1, x: 0, y: 0, scale: 1, ease: Bounce.easeOut }, '-=0').fromTo('#anim4-box-3', 1, { opacity: 0, x: -58, y: 76, scale: 0.5, transformOrigin: 'center' }, { opacity: 1, x: 0, y: 0, scale: 1, ease: Bounce.easeOut }, '-=0').fromTo('#anim4-box-4', 1, { opacity: 0, x: 62, y: 11, scale: 0.5, transformOrigin: 'center' }, { opacity: 1, x: 0, y: 0, scale: 1, ease: Bounce.easeOut }, '-=0').fromTo('#anim4-box-5', 1, { opacity: 0, x: 27, y: 11, scale: 0.5, transformOrigin: 'center' }, { opacity: 1, x: 0, y: 0, scale: 1, ease: Bounce.easeOut }, '-=0').fromTo('#anim4-box-6', 1, { opacity: 0, x: -9, y: 11, scale: 0.5, transformOrigin: 'center' }, { opacity: 1, x: 0, y: 0, scale: 1, ease: Bounce.easeOut }, '-=0');
 
 	scene4 = new ScrollMagic.Scene({
 		triggerElement: ".main__wrapper-2",
@@ -658,10 +562,6 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 	.addTo(controller4).on("progress", function (event) {
 		var num = Number((event.progress * 100).toFixed(0));
 
-		console.log("scene4 num: ", num);
-		// console.log($('#video2Wrapper').attr('style'));
-		// console.log($('#video2Wrapper').attr('style') === 'opacity: 0;');
-		//
 		if ($('#video2Wrapper').attr('style') === 'opacity: 0;') {
 			vidBool = true;
 			vid2.pause();
@@ -678,15 +578,7 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 		duration: wrapperNode4H - 200
 	}).setTween(tween41)
 	// .addIndicators({name: 'video'})
-	.addTo(controller41).on("progress", function (event) {
-		var num = Number((event.progress * 100).toFixed(0));
-
-		// console.log(`scene41 num: `, num);
-		//
-		// if(num <= 5) {
-		// 	if(vid2.paused) vid2.play();
-		// }
-	});
+	.addTo(controller41);
 
 	scene42 = new ScrollMagic.Scene({
 		triggerElement: ".main__wrapper-2",
@@ -701,57 +593,19 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 	/* SCENE 5
  * ==================== */
 	var tween5 = new TimelineMax(),
-	    tween51 = new TimelineMax(),
 	    tween52 = new TimelineMax(),
-	    tween53 = new TimelineMax(),
 	    scene5 = null,
-	    scene51 = null,
 	    scene52 = null,
-	    scene53 = null,
 	    controller5 = new ScrollMagic.Controller(),
-	    controller51 = new ScrollMagic.Controller(),
 	    controller52 = new ScrollMagic.Controller(),
-	    controller53 = new ScrollMagic.Controller(),
 	    vid3 = $('#video3')[0];
 
 	var wrapperNode5 = $('.main__wrapper-1'),
 	    wrapperNode5H = wrapperNode5.outerHeight(true);
 
-	tween5.to('#mainSVG3', 0.5, { opacity: 0 }, '-=0')
-	// .to(['#anim4-box-1', '#anim4-box-2', '#anim4-box-3', '#anim4-box-4', '#anim4-box-5', '#anim4-box-6'], 10,
-	// 	{opacity: 0, scale: 0.5, transformOrigin: 'center', ease: Linear.easeNone},
-	// 	'-=10'
-	// )
-	// .fromTo('#video3Wrapper', 1,
-	// 	{opacity: 0}, {opacity: 1, ease: Linear.easeNone}, '-=10'
-	// )
-	;
-
-	// tween51
-	// 	.fromTo('#cloud-border', 10,
-	// 		{opacity: 0, scale: 0.5, transformOrigin: 'center'}, {opacity: 1, scale: 1, ease: Linear.easeNone}, '-=0'
-	// 	)
-	// 	.fromTo('#cloud-fill', 10,
-	// 		{opacity: 0, scale: 0.85, transformOrigin: 'center'}, {opacity: 1, scale: 1, ease: Linear.easeNone}, '-=8'
-	// 	)
-	// 	.fromTo('#cloud-visual', 10,
-	// 		{opacity: 0, scale: 0, transformOrigin: 'center'}, {opacity: 0.3, scale: 1, ease: Elastic.easeOut.config(1.2,0.8)}, '-=7'
-	// 	)
-	// 	.fromTo('#cloud-circle-1', 10,
-	// 		{opacity: 0, scale: 0, transformOrigin: 'center'}, {opacity: 1, scale: 1, ease: Elastic.easeOut.config(1,0.4)}, '-=6'
-	// 	)
-	// 	.fromTo('#cloud-circle-2', 10,
-	// 		{opacity: 0, scale: 0, transformOrigin: 'center'}, {opacity: 1, scale: 1, ease: Elastic.easeOut.config(1,0.4)}, '-=6'
-	// 	)
-	// ;
+	tween5.to('#mainSVG3', 0.5, { opacity: 0 }, '-=0');
 
 	tween52.fromTo('#video3Wrapper', 1, { opacity: 0 }, { opacity: 1, ease: Linear.easeNone }, '-=0');
-
-	// tween53
-	// 	.to(['#video3Wrapper', '.main__wrapper-5 .main__wrapper-left > div'], 0.5,
-	// 		{opacity: 0, y: '-500px', ease: Linear.easeNone}, '-=0'
-	// 	)
-	// ;
 
 	scene5 = new ScrollMagic.Scene({
 		triggerElement: ".main__wrapper-1",
@@ -770,15 +624,6 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 		}
 	});
 
-	// scene51 = new ScrollMagic.Scene({
-	// 	triggerElement: ".main__wrapper-1",
-	// 	offset: 0,
-	// 	duration: wrapperNode5H - 100
-	// })
-	// 	.setTween(tween51)
-	// 	// .addIndicators({name: 'last-svg'})
-	// 	.addTo(controller51);
-
 	scene52 = new ScrollMagic.Scene({
 		triggerElement: ".main__wrapper-1",
 		offset: 200,
@@ -786,15 +631,6 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 	}).setTween(tween52)
 	// .addIndicators({name: 'last-svg'})
 	.addTo(controller52);
-
-	// scene53 = new ScrollMagic.Scene({
-	// 	triggerElement: ".main__wrapper-1",
-	// 	offset: $('.main__wrapper-1').outerHeight(true) / 1.075,
-	// 	duration: ($('.main__wrapper-1').outerHeight(true) - ($('.main__wrapper-1').outerHeight(true) / 1.075)) + 200
-	// })
-	// 	.setTween(tween53)
-	// .addIndicators({name: 'last-animation'})
-	// .addTo(controller53);
 
 	/* end :: SCENE 5
  * ==================== */
@@ -815,11 +651,7 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 	    controllerTXT4 = new ScrollMagic.Controller(),
 	    tweenTXT5 = new TimelineMax(),
 	    sceneTXT5 = null,
-	    controllerTXT5 = new ScrollMagic.Controller()
-	// tweenTXT6 = new TimelineMax(),
-	// sceneTXT6 = null,
-	// controllerTXT6 = new ScrollMagic.Controller()
-	;
+	    controllerTXT5 = new ScrollMagic.Controller();
 
 	tweenTXT1.fromTo('.main__wrapper-1 .main__wrapper-left', 1, { opacity: 1, y: 0 }, { opacity: 0, y: '-100%', ease: Linear.easeNone }, '-=0');
 
@@ -830,10 +662,6 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 	tweenTXT4.fromTo('.main__wrapper-3 .main__wrapper-left', 1, { opacity: 1 }, { opacity: 0, y: '-100%', ease: Linear.easeNone }, '-=0').fromTo('.main__wrapper-4 .main__wrapper-left', 1, { opacity: 0 }, { opacity: 1, y: 0, ease: Linear.easeNone }, '-=1');
 
 	tweenTXT5.fromTo('.main__wrapper-4 .main__wrapper-left', 1, { opacity: 1 }, { opacity: 0, y: '-100%', ease: Linear.easeNone }, '-=0').fromTo('.main__wrapper-5 .main__wrapper-left', 1, { opacity: 0 }, { opacity: 1, y: 0, ease: Linear.easeNone }, '-=1');
-
-	// tweenTXT6
-	// 	.to('.main__wrapper-5 .main__wrapper-left', 1, {opacity: 1, y: '-75%', ease: Linear.easeNone}, '-=0')
-	// ;
 
 	sceneTXT1 = new ScrollMagic.Scene({
 		triggerElement: ".main__wrapper-5",
@@ -874,15 +702,6 @@ var homepageMainScrollAnimation = function homepageMainScrollAnimation() {
 	}).setTween(tweenTXT5)
 	// .addIndicators({name: '5 text'})
 	.addTo(controllerTXT5);
-
-	// sceneTXT6 = new ScrollMagic.Scene({
-	// 	triggerElement: ".main__wrapper-1",
-	// 	offset: $('.main__wrapper-1').outerHeight(true) - 300,
-	// 	duration: 1000
-	// })
-	// 	.setTween(tweenTXT6)
-	// .addIndicators({name: '5 text'})
-	// .addTo(controllerTXT6);
 	/* end :: TEXT
  * ==================== */
 };
@@ -904,6 +723,7 @@ var headerChangeColor = function headerChangeColor() {
 
 		return vertInView && horInView;
 	}
+
 	function helperColorChange() {
 		if (isAnyPartOfElementInViewport(containerNode) && containerNode.getBoundingClientRect().top < 0) {
 			header.addClass('is-color');
@@ -920,10 +740,6 @@ var headerChangeColor = function headerChangeColor() {
 	$(window).on('scroll', function (ev) {
 		helperColorChange();
 	});
-
-	setTimeout(function () {
-		header.animate({ opacity: 1 }).css({ transform: 'translateY(0)' });
-	}, 500);
 };
 
 var approachCollapse = function approachCollapse() {
@@ -1000,4 +816,8 @@ var initNative = function initNative() {
 
 window.addEventListener('load', function () {
 	initNative();
+
+	setTimeout(function () {
+		$('#header').animate({ opacity: 1 });
+	}, 500);
 });
